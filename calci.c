fb0f20baadfd2b3a25f34d90f3e63465214b8cca
71202_Sanjana <sanjanadhtrak@gmail.com>
2023-06-06 13:45:59 +0530

    www

--- calci.c
+++ calci.c
@@ -32,19 +32,31 @@
 
 
 int main()
+//{
+   // int one, two, sub;
+
+   // printf("enter First Number: \n");
+   // scanf("%d", &one);
+    //printf("enter Second Number: \n");
+    //scanf("%d", &two);
+
+
+   // printf("First Num = %d, Second Num = %d \n",one, two);
+
+   // sub = one - two;
+
+   // printf("sub = %d", sub);
+   // return 0;
+//}
 {
-    int one, two, sub;
-
-    printf("enter First Number: \n");
-    scanf("%d", &one);
-    printf("enter Second Number: \n");
-    scanf("%d", &two);
-
-
-    printf("First Num = %d, Second Num = %d \n",one, two);
-
-    sub = one - two;
-
-    printf("sub = %d", sub);
+    int mul, num1, num2;
+    printf("enter first number: \n");
+    scanf("%d",&num1);
+    printf("enter second number:\n");
+    scanf("%d",&num2);
+    printf("first num = %d, Second num =  %d \n",num1,num2);
+    mul = num1 * num2;
+    printf("mul=%d",mul);
     return 0;
 }
+
