1570ba53329fe4cb524648e852d64d699c8a3b5f
71202_Sanjana <sanjanadhtrak@gmail.com>
2023-06-06 12:19:32 +0530

    rrr

--- calci.c
+++ calci.c
@@ -1,6 +1,16 @@
 #include<stdio.h>
 int main()
 {
+    int sum = 0;
+    int num = 12;
+   int num1 = 30;
+    scanf("num= %d",&num);
+    printf("output=%d\n",num);
+    scanf("num1=%d",&num1);
+    printf("output=%d\n",num1);
+    sum=num+num1;
+    printf("addition=%d",sum);
+
     printf("hello world\n"); 
     // this comment was added by Prafull
     return 0;
